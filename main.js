231017df56a6df554c263ce1dd3ef1d23d72610d
Hampus Bosson <hampus.bosson@outlook.com>
2023-10-06 18:28:45 +0200

    Add functionality to display numbers in the display when buttons are clicked

--- main.js
+++ main.js
@@ -1,7 +1,59 @@
-const num1 = 0; 
-const num2 = 0; 
-const operator = ''; 
+let num1 = 0; 
+let num2 = 0; 
+let operator = ''; 
+let numbers1 = [];
+let numbers2 = []; 
+let equalsActive = false;
+let opActive = false; 
 
+
+const buttons = document.querySelectorAll('.button');
+const lastOperation = document.getElementById('upper-display');
+const currentOperation = document.getElementById('lower-display'); 
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', function(){
+        const number = button.textContent;
+        displayNumbers(number);
+        storeNumbers(number);
+        if(opActive == true) {
+            console.log(numbers1);
+        }
+
+    });
+});
+
+operators.forEach(op => {
+    op.addEventListener('click', function() {
+        opActive = true; 
+        lastOperation.textContent = numbers1.join('') + ' ' + op.textContent;
+    })
+});
+
+equals.addEventListener('click', function() {
+    equalsActive = true
+});
+
+
+
+function displayNumbers (number) {
+    if (/^\d+$/.test(number) || number == '.' && !currentOperation.textContent.includes('.')) {
+        currentOperation.textContent += number;
+    } else if (/^\d+$/.test(number)) {
+        currentOperation.textContent += number;
+    } 
+}
+
+function storeNumbers(value) {
+    if (/^\d+$/.test(value) || value == '.') {
+        numbers1.push(value);
+    } 
+
+
+}
 
 function add(num1, num2) {
     return num1 + num2;
