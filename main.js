02cc05fee1044753c01aad92a525fecbf0d42776
Hampus Bosson <hampus.bosson@outlook.com>
2023-10-10 10:48:25 +0200

    Add functionality to calculate both with the operators and the equal sign and make them function togheter

--- main.js
+++ main.js
@@ -1,59 +1,97 @@
-let num1 = 0; 
-let num2 = 0; 
-let operator = ''; 
-let numbers1 = [];
-let numbers2 = []; 
-let equalsActive = false;
-let opActive = false; 
+let firstNum = 0;
+let currentNum = 0;
+let currentOperator = ''; 
+let result = '';
+let shouldResetScreen = false; 
+let evaluatePressed = false; 
 
 
 const buttons = document.querySelectorAll('.button');
 const lastOperation = document.getElementById('upper-display');
 const currentOperation = document.getElementById('lower-display'); 
 const operators = document.querySelectorAll('.operator');
-const equals = document.querySelector('.equals');
+const equalsButton = document.querySelector('.equals');
+const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
+
+//clearButton.addEventListener('click', clearDisplay());
+
+//deleteButton.addEventListener('click', deleteNumber());
+
+equalsButton.addEventListener('click', () => evaluate());
 
 
 buttons.forEach(button => {
-    button.addEventListener('click', function(){
-        const number = button.textContent;
-        displayNumbers(number);
-        storeNumbers(number);
-        if(opActive == true) {
-            console.log(numbers1);
-        }
-
-    });
+    button.addEventListener('click', () => displayNumbers(button.textContent));
 });
+
 
 operators.forEach(op => {
-    op.addEventListener('click', function() {
-        opActive = true; 
-        lastOperation.textContent = numbers1.join('') + ' ' + op.textContent;
-    })
-});
-
-equals.addEventListener('click', function() {
-    equalsActive = true
+    op.addEventListener('click', () => setOperation(op.textContent)); 
 });
 
 
 
-function displayNumbers (number) {
-    if (/^\d+$/.test(number) || number == '.' && !currentOperation.textContent.includes('.')) {
+function displayNumbers(number) {
+    if (/^\d+$/.test(number) || number == '.') {
+        if (number === '.' && currentOperation.textContent.includes('.')) {
+            return;
+        } else if (shouldResetScreen) { 
+            currentOperation.textContent = '';
+            shouldResetScreen = false; 
+        }
         currentOperation.textContent += number;
-    } else if (/^\d+$/.test(number)) {
-        currentOperation.textContent += number;
-    } 
+    }
 }
 
-function storeNumbers(value) {
-    if (/^\d+$/.test(value) || value == '.') {
-        numbers1.push(value);
-    } 
 
+function setOperation(operator) {
+    shouldResetScreen = true; 
+    currentNum = parseFloat(currentOperation.textContent);
+    if (lastOperation.textContent === '') {
+        if(operator == '×' || operator == '÷') { 
+            currentOperator = operator;
+            result = parseFloat(operate(operator, currentNum, 1))
+            lastOperation.textContent = result + ' ' + operator;
+        } else {
+        currentOperator = operator;
+        result = parseFloat(operate(operator, currentNum, 0))
+        lastOperation.textContent = result + ' ' + operator;
+    }
 
+    } else {
+        currentOperator = operator;
+        firstNum = parseFloat(result);
+        result = parseFloat(operate(currentOperator, firstNum, currentNumValue()));
+        lastOperation.textContent = parseFloat(result) + ' ' + operator; 
+        evaluatePressed = false; 
+    }
 }
+
+function evaluate() {
+    evaluatePressed = true;
+    firstNum = result; 
+    currentNum = parseFloat(currentOperation.textContent); 
+    
+    result = parseFloat(operate(currentOperator, firstNum, currentNum)); 
+
+    currentOperation.textContent = parseFloat(result); 
+    lastOperation.textContent = `${firstNum} ${currentOperator} ${currentNum} =`;
+}
+
+function currentNumValue() {
+    if(evaluatePressed && (currentOperator == '×' || currentOperator == '÷')) {
+        return 1;
+    } else if ((evaluatePressed && result < 0) && (currentOperator == '+' || currentOperator == '-')) {
+        return 0; 
+    } else {
+        let num = parseFloat(currentOperation.textContent);
+        return num; 
+    }
+}
+
+
+
 
 function add(num1, num2) {
     return num1 + num2;
@@ -71,16 +109,26 @@ function divide(num1, num2) {
     return num1 / num2; 
 }
 
-function operate(operator, num1, num2){
+function operate(operator, num1, num2) {
+    let result;
     switch(operator) {
         case '+':
-            return add(num1, num2);
+            result = add(num1, num2);
+            break;
         case '-':
-            return subtract(num1, num2);
-        case '*':
-            return multiply(num1, num2);
-        case '/':
-            return divide(num1, num2);
+            result = subtract(num1, num2);
+            break;
+        case '×':
+            result = multiply(num1, num2);
+            break;
+        case '÷':
+            if (num2 === 0) {
+                // Handle division by zero if necessary
+                return "Error";
+            }
+            result = divide(num1, num2);
+            break;
     }
+    return parseFloat(result.toFixed(3)); // Round to two decimal places
 }
 
